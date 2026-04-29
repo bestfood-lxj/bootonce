79fd8ea7ddd77e10809e77fc2b0d7a05ac506dd8
bestfood-lxj <lixj@321tips.com>
2026-04-29 18:46:16 +0800

    :poop:

--- abce/src/components/Fditor.vue
+++ abce/src/components/Fditor.vue
@@ -19,21 +19,66 @@
 import '@wangeditor-next/editor/dist/css/style.css' // 引入 css
 
 import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
-import { Editor, Toolbar } from '@wangeditor-next/editor-for-vue'
+import { Boot, SlateEditor, SlateTransforms, } from '@wangeditor-next/editor';
+import { Editor, Toolbar,  } from '@wangeditor-next/editor-for-vue'
+import { h } from 'snabbdom';
 
 export default {
   components: { Editor, Toolbar },
   setup() {
+
+    const renderIcon = (elemNode: any) => {
+      const {
+        icon,
+        size = 16,
+        color = 'inherit',
+        svgClass = '',
+        onClick = () => { },
+      } = elemNode;
+      return h(
+        'i',
+        {
+          attrs: {
+            class: 'el-icon v-icon',
+            contenteditable: false,
+            style: `font-size: ${size}px;color: ${color}`,
+          },
+          props: {
+            onClick: onClick,
+          },
+        },
+      )
+    }
     // 编辑器实例，必须用 shallowRef
     const editorRef = shallowRef()
 
     // 内容 HTML
     const valueHtml = ref('<p>hello</p>')
 
+    Boot.registerModule({
+      renderElems: [
+        {
+          type: 'icon',
+          renderElem: renderIcon,
+        },
+      ],
+    });
+
     // 模拟 ajax 异步获取内容
     onMounted(() => {
+        const node1 = { type: 'paragraph', children: [{ text: 'aaa' }] }
+        const node2 = { type: 'paragraph', children: [{ text: 'bbb' }] }
+        const nodeList = [node1, node2]
+
+        
+        console.log(editorRef.value,editorRef)
         setTimeout(() => {
-            valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
+          SlateTransforms.insertNodes(
+            editorRef.value, 
+            nodeList,
+            { at: SlateEditor.end(editorRef.value, []) }
+          )
+            //valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
         }, 1500)
     })
 
