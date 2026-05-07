7774d7e66f3263e83203f347aa9378a347c68aca
bestfood-lxj <lixj@321tips.com>
2026-05-07 16:38:52 +0800

    :poop:

--- abce/src/components/Fditor.vue
+++ abce/src/components/Fditor.vue
@@ -31,22 +31,7 @@ function buildVNode(isUpdated) {
   ]);
 }
 onMounted(()=>{
-  const container = document.getElementById("container");
-  const vnode = h("div", [
-    h("svg", { attrs: { width: 100, height: 100 } }, [
-      h("circle", {
-        attrs: {
-          cx: 50,
-          cy: 50,
-          r: 40,
-          stroke: "green",
-          "stroke-width": 4,
-          fill: "yellow"
-        }
-      })
-    ])
-  ]);
-  patch(container, vnode);
+ 
   patchVue(mountEl.value, buildVNode(false))
 })
 </script>
