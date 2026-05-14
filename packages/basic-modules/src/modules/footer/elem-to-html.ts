5402f28d1a06b61914056e9b78f22985675bdda5
bestfood-lxj <lixj@321tips.com>
2026-05-14 16:45:41 +0800

    :poop:

--- packages/basic-modules/src/modules/footer/elem-to-html.ts
+++ packages/basic-modules/src/modules/footer/elem-to-html.ts
@@ -4,29 +4,12 @@
  */
 
 import { Element } from 'slate'
-import { init, h, classModule, styleModule, eventListenersModule, attributesModule } from 'snabbdom'
 
 function pToHtml(elem: Element, childrenHtml: string): string {
-	const patchVue = init([attributesModule,classModule, styleModule, eventListenersModule,])
   if (childrenHtml === '') {
     return '<footer><br></footer>'
   }
-  h("div", [
-    h("svg", { attrs: { width: 100, height: 100 } }, [
-      h("circle", {
-        attrs: {
-          cx: 50,
-          cy: 50,
-          r: 40,
-          stroke: "green",
-          "stroke-width": 4,
-          fill: "yellow"
-        }
-      })
-    ]),
-    h("span", { style: { fontWeight: "bold" } }, "This is bold " + childrenHtml),
-  ]);
-  //return `<footer>${childrenHtml}</footer>`
+  return `<footer>${childrenHtml}</footer>`
 }
 
 export const pToHtmlConf = {
