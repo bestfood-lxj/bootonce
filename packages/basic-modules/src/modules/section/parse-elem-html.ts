ec0ada5921c9a09bc013a6ae3a3a5b1d0d3a2c67
bestfood-lxj <lixj@321tips.com>
2026-05-19 18:48:51 +0800

    :poop:

--- packages/basic-modules/src/modules/section/parse-elem-html.ts
+++ packages/basic-modules/src/modules/section/parse-elem-html.ts
@@ -7,34 +7,7 @@ import { IDomEditor } from '@wangeditor-next/core'
 import { Descendant, Text } from 'slate'
 
 
-import $, {
-  addClass,
-  append,
-  attr,
-  children,
-  css,
-  dataset,
-  Dom7Array,
-  empty,
-  filter,
-  find,
-  focus,
-  hasClass,
-  height,
-  hide,
-  html,
-  off,
-  on,
-  parents,
-  prepend,
-  remove,
-  removeAttr,
-  removeClass,
-  show,
-  text,
-  val,
-  width,
-} from 'dom7'
+import $, { DOMElement } from '../../utils/dom'
 
 // COMPAT: This is required to prevent TypeScript aliases from doing some very
 // weird things for Slate's types with the same name as globals. (2019/11/27)
@@ -47,38 +20,8 @@ import DOMRange = globalThis.Range
 import DOMSelection = globalThis.Selection
 import DOMStaticRange = globalThis.StaticRange
 
-import $ from 'dom7'
-
-
-
-import { ParagraphElement } from './custom-types'
-if (css) { $.fn.css = css }
-if (append) { $.fn.append = append }
-if (prepend) { $.fn.prepend = prepend }
-if (addClass) { $.fn.addClass = addClass }
-if (removeClass) { $.fn.removeClass = removeClass }
-if (hasClass) { $.fn.hasClass = hasClass }
-if (on) { $.fn.on = on }
-if (off) { $.fn.off = off }
-if (focus) { $.fn.focus = focus }
-if (attr) { $.fn.attr = attr }
-if (removeAttr) { $.fn.removeAttr = removeAttr }
-if (hide) { $.fn.hide = hide }
-if (show) { $.fn.show = show }
-if (parents) { $.fn.parents = parents }
-if (dataset) { $.fn.dataset = dataset }
-if (val) { $.fn.val = val }
-if (text) { $.fn.text = text }
-if (html) { $.fn.html = html }
-if (children) { $.fn.children = children }
-if (remove) { $.fn.remove = remove }
-if (find) { $.fn.find = find }
-if (width) { $.fn.width = width }
-if (height) { $.fn.height = height }
-if (filter) { $.fn.filter = filter }
-if (empty) { $.fn.empty = empty }
 function parseParagraphHtml(
-  elem: any,
+  elem: DOMElement,
   children: Descendant[],
   editor: IDomEditor,
 ): ParagraphElement {
