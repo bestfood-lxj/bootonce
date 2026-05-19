01b63dbbb6621dfbaa59fdad03fc56cf90bfa52c
bestfood-lxj <lixj@321tips.com>
2026-05-19 17:43:54 +0800

    :poop:

--- apps/demo-vue-a/src/header/index.ts
+++ apps/demo-vue-a/src/header/index.ts
@@ -6,14 +6,13 @@
 import { IModuleConf } from '@wangeditor-next/core'
 
 import { pToHtmlConf } from './elem-to-html'
-import { parseParagraphHtmlConf } from './parse-elem-html'
+
 import withParagraph from './plugin'
 import { renderParagraphConf } from './render-elem'
 
 const p: Partial<IModuleConf> = {
   renderElems: [renderParagraphConf],
   elemsToHtml: [pToHtmlConf],
-  parseElemsHtml: [parseParagraphHtmlConf],
   editorPlugin: withParagraph,
 }
 
