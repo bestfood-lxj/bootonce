313e0dd0b85962a78100984270e3932c14802863
bestfood-lxj <lixj@321tips.com>
2026-06-17 14:15:50 +0800

    :poop:

--- examples/interactive-docs/example.js
+++
@@ -1,5 +0,0 @@
-
-global.React = require('react');
-global.ReactDOM = require('react-dom');
-global.Animated = require('../../lib/targets/react-dom');
-global.createClass = require("create-react-class");
