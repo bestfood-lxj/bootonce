9e28025c0c3766236e766ece1d344561ab7be367
bestfood-lxj <lixj@321tips.com>
2026-06-17 14:32:14 +0800

    :poop:

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,22 +1,32 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { useState } from 'react';
+import ReactiveButton from 'reactive-button';
 
 export function Welcome() {
+  const [state, setState] = useState('idle');
+
+  const onClickHandler = () => {
+    setState('loading');
+
+    // send an HTTP request
+    setTimeout(() => {
+      setState('success');
+    }, 2000);
+  };
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
+            <ReactiveButton
+                buttonState={state}
+                idleText="Submit"
+                loadingText="Loading"
+                successText="Done"
+                onClick={onClickHandler}
+              />
           </div>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
