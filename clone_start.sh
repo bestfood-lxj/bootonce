3713d116411cbae2b73d5f44e55ef21d5973eecb
bestfood-lxj <lixj@321tips.com>
2026-07-28 16:48:43 +0800

    :poop:

---
+++ clone_start.sh
@@ -0,0 +1,10 @@
+# https://github.com/hahhforest/pi-textbook
+# https://build-your-own-pi-cn.enochzhang.chatgpt.site/
+# 
+
+
+git clone --branch course/build-your-own-pi https://github.com/hahhforest/pi.git
+cd pi
+npm install
+npm run checkpoint -w @pi/course -- 00
+npm run practice -w @pi/course -- 00 ../pi-practice-00
